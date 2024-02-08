793399e1729b05a36b37af6e2f29c5672565018e
biluadev <biluadevcod@gmail.com>
2024-02-08 11:25:14 +0100

    Finalizado a home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,10 +7,28 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="">Vou trabalhar em</label>
-                    <TaskInput id="task" placeholder="Dê um nome para o seu projeto"/>
+                    <TaskInput 
+                        id="task" 
+                        list="task-suggestions"
+                        placeholder="Dê um nome para o seu projeto"
+                    />
 
-                    <label htmlFor="">Vou trabalhar em</label>
-                    <MinutesAmountInput type="number" id="minutesAmount" />
+                    <datalist id="task-suggestions">
+                        <option value="projeto 1"/>
+                        <option value="projeto 2"/>
+                        <option value="projeto 3"/>
+                        <option value="Ignite"/>
+                    </datalist>
+
+                    <label htmlFor="">durante</label>
+                    <MinutesAmountInput 
+                        type="number" 
+                        id="minutesAmount" 
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60} 
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
